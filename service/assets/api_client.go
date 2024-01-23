984952e7febc7ce6be0004974a687965aebff99b
vingurzhou <zwz0123460218@icloud.com>
2024-01-23 17:41:29 +0800

    feat: sdk
    - GetMintNFTResult
    - GetTransferNFTResult

--- service/assets/api_client.go
+++ service/assets/api_client.go
@@ -54,7 +54,6 @@ type CreateArtworkRequest struct {
 	// 请求id
 	RequestId string `protobuf:"bytes,10,opt,name=request_id,json=requestId,proto3" json:"request_id,omitempty"`
 }
-
 type CreateArtworkResponse struct {
 	// 合约地址
 	ContractAddress string `protobuf:"bytes,1,opt,name=contract_address,json=contractAddress,proto3" json:"contract_address,omitempty"`
@@ -63,21 +62,153 @@ type CreateArtworkResponse struct {
 	// 交易状态
 	Status string `protobuf:"bytes,3,opt,name=status,proto3" json:"status,omitempty"`
 }
-
 type GetArtworkResultRequest struct {
 	// 交易哈希
 	Txhash string `protobuf:"bytes,1,opt,name=txhash,proto3" json:"txhash,omitempty"`
 	// 租户id
 	TenantId string `protobuf:"bytes,3,opt,name=tenant_id,json=tenantId,proto3" json:"tenant_id,omitempty"`
 }
-
 type GetArtworkResultResponse struct {
 	// 状态
 	Status string `protobuf:"bytes,1,opt,name=status,proto3" json:"status,omitempty"`
 	// 合约地址
 	ContractAddress string `protobuf:"bytes,2,opt,name=contract_address,json=contractAddress,proto3" json:"contract_address,omitempty"`
 }
+type GetMintNFTResultRequest struct {
+	// 交易哈希
+	TxHash string `protobuf:"bytes,2,opt,name=tx_hash,json=txHash,proto3" json:"tx_hash,omitempty"`
+	// 租户id
+	TenantId string `protobuf:"bytes,3,opt,name=tenant_id,json=tenantId,proto3" json:"tenant_id,omitempty"`
+}
+type GetMintNFTResultResponse struct {
+	// token id
+	TokenIds []string `protobuf:"bytes,1,rep,name=token_ids,json=tokenIds,proto3" json:"token_ids,omitempty"`
+	// 钱包地址
+	WalletAddress string `protobuf:"bytes,2,opt,name=wallet_address,json=walletAddress,proto3" json:"wallet_address,omitempty"`
+	// 状态
+	Status string `protobuf:"bytes,3,opt,name=status,proto3" json:"status,omitempty"`
+}
+type GetTransferNFTResultRequest struct {
+	// 交易哈希
+	TxHash string `protobuf:"bytes,1,opt,name=tx_hash,json=txHash,proto3" json:"tx_hash,omitempty"`
+	// 租户id
+	TenantId string `protobuf:"bytes,2,opt,name=tenant_id,json=tenantId,proto3" json:"tenant_id,omitempty"`
+}
+type GetTransferNFTResultResponse struct {
+	// 状态
+	Status string `protobuf:"bytes,1,opt,name=status,proto3" json:"status,omitempty"`
+}
+type MintNFTRequest struct {
+	// 合约地址
+	ContractAddress string `protobuf:"bytes,1,opt,name=contract_address,json=contractAddress,proto3" json:"contract_address,omitempty"`
+	// 所有者
+	ReceiverAddress string `protobuf:"bytes,2,opt,name=receiver_address,json=receiverAddress,proto3" json:"receiver_address,omitempty"`
+	// 签名者
+	Signer *Signer `protobuf:"bytes,3,opt,name=signer,proto3" json:"signer,omitempty"`
+	// 租户id
+	TenantId string `protobuf:"bytes,4,opt,name=tenant_id,json=tenantId,proto3" json:"tenant_id,omitempty"`
+	// 数量
+	Amount string `protobuf:"bytes,5,opt,name=amount,proto3" json:"amount,omitempty"`
+	// 请求id
+	RequestId string `protobuf:"bytes,6,opt,name=request_id,json=requestId,proto3" json:"request_id,omitempty"`
+}
+type MintNFTResponse struct {
+	// token id
+	TokenIds []string `protobuf:"bytes,1,rep,name=token_ids,json=tokenIds,proto3" json:"token_ids,omitempty"`
+	// 交易哈希
+	TxHash string `protobuf:"bytes,2,opt,name=tx_hash,json=txHash,proto3" json:"txhash,omitempty"`
+	// 交易状态
+	Status string `protobuf:"bytes,3,opt,name=status,proto3" json:"status,omitempty"`
+	// 钱包地址
+	ContractAddress string `protobuf:"bytes,4,opt,name=contract_address,json=contractAddress,proto3" json:"contract_address,omitempty"`
+}
+type TransferNFTRequest struct {
+	// 新所有者
+	ReceiverAddress string `protobuf:"bytes,2,opt,name=receiver_address,json=receiverAddress,proto3" json:"receiver_address,omitempty"`
+	// 合约地址
+	ContractAddress string `protobuf:"bytes,3,opt,name=contract_address,json=contractAddress,proto3" json:"contract_address,omitempty"`
+	// token id
+	TokenId string `protobuf:"bytes,4,opt,name=token_id,json=tokenId,proto3" json:"token_id,omitempty"`
+	// 签名者
+	Signer *Signer `protobuf:"bytes,5,opt,name=signer,proto3" json:"signer,omitempty"`
+	// 租户id
+	TenantId string `protobuf:"bytes,6,opt,name=tenant_id,json=tenantId,proto3" json:"tenant_id,omitempty"`
+	// 请求id
+	RequestId string `protobuf:"bytes,7,opt,name=request_id,json=requestId,proto3" json:"request_id,omitempty"`
+}
+type TransferNFTResponse struct {
+	// 交易哈希
+	TxHash string `protobuf:"bytes,1,opt,name=tx_hash,json=txHash,proto3" json:"txhash,omitempty"`
+	// 交易状态
+	Status string `protobuf:"bytes,2,opt,name=status,proto3" json:"status,omitempty"`
+}
 
+func (r *CreateArtworkRequest) Payload() string {
+	b := strings.Builder{}
+	s1 := fmt.Sprintf("name=\"%s\"", r.Name)
+	s2 := fmt.Sprintf("symbol=\"%s\"", r.Symbol)
+	s3 := fmt.Sprintf("artwork_url=\"%s\"", r.ArtworkUrl)
+	s4 := fmt.Sprintf("digest=\"%s\"", r.Digest)
+	s5 := fmt.Sprintf("max_supply=\"%s\"", r.MaxSupply)
+	s6 := fmt.Sprintf("tenant_id=\"%s\"", r.TenantId)
+	s7 := fmt.Sprintf("request_id=\"%s\"", r.RequestId)
+	s8 := fmt.Sprintf("signer={\"signed_user_id\":\"%s\",\"wallet_id\":\"%s\"}", r.Signer.SignedUserId, r.Signer.WalletId)
+	s := strings.Join([]string{s3, s4, s5, s1, s7, s8, s2, s6}, "&")
+	b.WriteString(s)
+	return b.String()
+}
+
+func (r *GetArtworkResultRequest) Payload() string {
+	b := strings.Builder{}
+	s1 := fmt.Sprintf("txhash=\"%s\"", r.Txhash)
+	s2 := fmt.Sprintf("tenant_id=\"%s\"", r.TenantId)
+	s := strings.Join([]string{s2, s1}, "&")
+	b.WriteString(s)
+	return b.String()
+}
+
+func (r *GetMintNFTResultRequest) Payload() string {
+	b := strings.Builder{}
+	s1 := fmt.Sprintf("tx_hash=\"%s\"", r.TxHash)
+	s2 := fmt.Sprintf("tenant_id=\"%s\"", r.TenantId)
+	s := strings.Join([]string{s2, s1}, "&")
+	b.WriteString(s)
+	return b.String()
+}
+
+func (r *GetTransferNFTResultRequest) Payload() string {
+	b := strings.Builder{}
+	s1 := fmt.Sprintf("tx_hash=\"%s\"", r.TxHash)
+	s2 := fmt.Sprintf("tenant_id=\"%s\"", r.TenantId)
+	s := strings.Join([]string{s2, s1}, "&")
+	b.WriteString(s)
+	return b.String()
+}
+
+func (r *MintNFTRequest) Payload() string {
+	b := strings.Builder{}
+	s1 := fmt.Sprintf("receiver_address=\"%s\"", r.ReceiverAddress)
+	s2 := fmt.Sprintf("contract_address=\"%s\"", r.ContractAddress)
+	s3 := fmt.Sprintf("amount=\"%s\"", r.Amount)
+	s4 := fmt.Sprintf("tenant_id=\"%s\"", r.TenantId)
+	s5 := fmt.Sprintf("request_id=\"%s\"", r.RequestId)
+	s6 := fmt.Sprintf("signer={\"signed_user_id\":\"%s\",\"wallet_id\":\"%s\"}", r.Signer.SignedUserId, r.Signer.WalletId)
+	s := strings.Join([]string{s3, s2, s1, s5, s6, s4}, "&")
+	b.WriteString(s)
+	return b.String()
+}
+func (r *TransferNFTRequest) Payload() string {
+	b := strings.Builder{}
+	s1 := fmt.Sprintf("receiver_address=\"%s\"", r.ReceiverAddress)
+	s2 := fmt.Sprintf("contract_address=\"%s\"", r.ContractAddress)
+	s3 := fmt.Sprintf("token_id=\"%s\"", r.TokenId)
+	s4 := fmt.Sprintf("tenant_id=\"%s\"", r.TenantId)
+	s5 := fmt.Sprintf("request_id=\"%s\"", r.RequestId)
+	s6 := fmt.Sprintf("signer={\"signed_user_id\":\"%s\",\"wallet_id\":\"%s\"}", r.Signer.SignedUserId, r.Signer.WalletId)
+	s := strings.Join([]string{s2, s1, s5, s6, s4, s3}, "&")
+	b.WriteString(s)
+	return b.String()
+}
 func NewClient(ctx context.Context, cfg *Config) (*Client, error) {
 	if cfg.AK == "" || cfg.SK == "" || cfg.Endpoint == "" || cfg.TenantId == "" {
 		return nil, errors.New("cfg ak,sk,tenantId,endpoint can not be empty")
@@ -122,30 +253,58 @@ func (c *Client) GetArtworkResult(ctx context.Context, in *GetArtworkResultReque
 	return out, nil
 }
 
-// model_name="jingkuang-nft"&name_values=[{"key":"contractaddress","value":"0x13FF557D9578f2Fd78fF47383184A70e685b600f"},
-// {"key":"zichanmingcheng","value":"雨水卡"},{"key"huangzuozhe","value":"版易官方平台-004"},
-// {"key":"faxingliang","value":"100"},{"key":"faxingshijian","value":"2023-07-17 18:55:15"},
-// {"key":"liulanqidizhi","value":"httpslorer.jingkuang.info/address/0x13FF557D9578f2Fd78fF47383184A70e685b600f"}]&tenant_id="tid-yuhu1"
-func (r *CreateArtworkRequest) Payload() string {
-	b := strings.Builder{}
-	s1 := fmt.Sprintf("name=\"%s\"", r.Name)
-	s2 := fmt.Sprintf("symbol=\"%s\"", r.Symbol)
-	s3 := fmt.Sprintf("artwork_url=\"%s\"", r.ArtworkUrl)
-	s4 := fmt.Sprintf("digest=\"%s\"", r.Digest)
-	s5 := fmt.Sprintf("max_supply=\"%s\"", r.MaxSupply)
-	s6 := fmt.Sprintf("tenant_id=\"%s\"", r.TenantId)
-	s7 := fmt.Sprintf("request_id=\"%s\"", r.RequestId)
-	s8 := fmt.Sprintf("signer={\"signed_user_id\":\"%s\",\"wallet_id\":\"%s\"}", r.Signer.SignedUserId, r.Signer.WalletId)
-	s := strings.Join([]string{s3, s4, s5, s1, s7, s8, s2, s6}, "&")
-	b.WriteString(s)
-	return b.String()
+func (c *Client) GetMintNFTResult(ctx context.Context, in *GetMintNFTResultRequest, opts ...qhttp.CallOption) (*GetMintNFTResultResponse, error) {
+	out := new(GetMintNFTResultResponse)
+	pattern := "/v1/app/nfts:mint/result"
+	path := fmt.Sprintf("/v1/app/nfts:mint/result?tx_hash=%s&tenant_id=%s", in.TxHash, in.TenantId)
+
+	opts = append(opts, qhttp.Operation("qilin.api.assets.GetMintNFTResult"))
+	opts = append(opts, qhttp.PathTemplate(pattern))
+	err := c.cc.Invoke(ctx, "GET", path, in, &out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
 }
 
-func (r *GetArtworkResultRequest) Payload() string {
-	b := strings.Builder{}
-	s1 := fmt.Sprintf("txhash=\"%s\"", r.Txhash)
-	s2 := fmt.Sprintf("tenant_id=\"%s\"", r.TenantId)
-	s := strings.Join([]string{s2, s1}, "&")
-	b.WriteString(s)
-	return b.String()
+func (c *Client) GetTransferNFTResult(ctx context.Context, in *GetTransferNFTResultRequest, opts ...qhttp.CallOption) (*GetTransferNFTResultResponse, error) {
+	out := new(GetTransferNFTResultResponse)
+	pattern := "/v1/app/nfts:transfer/result"
+	path := fmt.Sprintf("/v1/app/nfts:transfer/result?tx_hash=%s&tenant_id=%s", in.TxHash, in.TenantId)
+
+	opts = append(opts, qhttp.Operation("qilin.api.assets.GetMintNFTResult"))
+	opts = append(opts, qhttp.PathTemplate(pattern))
+	err := c.cc.Invoke(ctx, "GET", path, in, &out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *Client) MintNFT(ctx context.Context, in *MintNFTRequest, opts ...qhttp.CallOption) (*MintNFTResponse, error) {
+	out := new(MintNFTResponse)
+	pattern := "/v1/app/nfts:mint"
+	path := "/v1/app/nfts:mint"
+
+	opts = append(opts, qhttp.Operation("qilin.api.assets.MintNFT"))
+	opts = append(opts, qhttp.PathTemplate(pattern))
+	err := c.cc.Invoke(ctx, "POST", path, in, &out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *Client) TransferNFT(ctx context.Context, in *TransferNFTRequest, opts ...qhttp.CallOption) (*TransferNFTResponse, error) {
+	out := new(TransferNFTResponse)
+	pattern := "/v1/app/nfts:transfer"
+	path := "/v1/app/nfts:transfer"
+
+	opts = append(opts, qhttp.Operation("qilin.api.assets.TransferNFT"))
+	opts = append(opts, qhttp.PathTemplate(pattern))
+	err := c.cc.Invoke(ctx, "POST", path, in, &out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
 }
--- service/assets/api_client_test.go
+++ service/assets/api_client_test.go
@@ -6,16 +6,19 @@ package assets
 
 //
 //import (
-//	"context"
 //	"testing"
+//)
+//
+//import (
+//	"context"
 //)
 //
 //const (
 //	TestTenant = "tid-yuhu1"
 //	Ak         = "test-ak"
 //	Sk         = "test-sk"
-//	// dev endpoint
-//	Endpoint = "localhost:10000"
+//	Endpoint   = "119.3.106.151:10100"
+//	//Endpoint = "127.0.0.1:20000"
 //)
 //
 //var cli *Client
@@ -32,6 +35,7 @@ package assets
 //	ctx = context.Background()
 //	m.Run()
 //}
+//
 //func TestClient_CreateArtwork(t *testing.T) {
 //	createArtworkResponse, err := cli.CreateArtwork(ctx, &CreateArtworkRequest{
 //		Name:       "name7",
@@ -52,7 +56,7 @@ package assets
 //	t.Log(createArtworkResponse)
 //}
 //
-//func TestGetArtworkResultRequest_Payload(t *testing.T) {
+//func TestClient_GetArtworkResult(t *testing.T) {
 //	getArtworkResultResponse, err := cli.GetArtworkResult(ctx, &GetArtworkResultRequest{
 //		Txhash:   "872fdbcfd27f44758385705ee7ffb5784afffdd468e44bbda2714a07c1759b0d",
 //		TenantId: "tid-yuhu1",
@@ -61,4 +65,62 @@ package assets
 //		t.Error(err)
 //	}
 //	t.Log(getArtworkResultResponse)
+//}
+//
+//func TestClient_GetMintNFTResult(t *testing.T) {
+//	getMintNFTResultResponse, err := cli.GetMintNFTResult(ctx, &GetMintNFTResultRequest{
+//		TxHash:   "8518e4a79ce346838ab3dc61829ac5d4197a865fb8654e4e84b19eca852d308c",
+//		TenantId: "tid-yuhu1",
+//	})
+//	if err != nil {
+//		t.Error(err)
+//	}
+//	t.Log(getMintNFTResultResponse)
+//}
+//
+//func TestClient_GetTransferNFTResult(t *testing.T) {
+//	getTransferNFTResultResponse, err := cli.GetTransferNFTResult(ctx, &GetTransferNFTResultRequest{
+//		TxHash:   "dddc644c5467403a99d7ff9702bcb0d93bfb58cc408347b19e69e1f532c3e18a",
+//		TenantId: "tid-yuhu1",
+//	})
+//	if err != nil {
+//		t.Error(err)
+//	}
+//	t.Log(getTransferNFTResultResponse)
+//}
+//
+//func TestClient_MintNFT(t *testing.T) {
+//	mintNFTResponse, err := cli.MintNFT(ctx, &MintNFTRequest{
+//		ContractAddress: "74a55fb59f51faba6fdc8ac94e1706680cb7b622",
+//		ReceiverAddress: "0x7dbd5d3efb0c583257167b1efd58af562053b16c",
+//		Signer: &Signer{
+//			WalletId:     "wid-llw34jmEw8vE",
+//			SignedUserId: "yuhu1",
+//		},
+//		TenantId:  "tid-yuhu1",
+//		Amount:    "3",
+//		RequestId: "202401242",
+//	})
+//	if err != nil {
+//		t.Error(err)
+//	}
+//	t.Log(mintNFTResponse)
+//}
+//
+//func TestClient_TransferNFT(t *testing.T) {
+//	transferNFTResponse, err := cli.TransferNFT(ctx, &TransferNFTRequest{
+//		ReceiverAddress: "0xec2d2fcf440de78163dc7dc95a46520c0114867b",
+//		ContractAddress: "74a55fb59f51faba6fdc8ac94e1706680cb7b622",
+//		TokenId:         "10",
+//		Signer: &Signer{
+//			WalletId:     "wid-llw34jmEw8vE",
+//			SignedUserId: "yuhu1",
+//		},
+//		TenantId:  "tid-yuhu1",
+//		RequestId: "202401232",
+//	})
+//	if err != nil {
+//		t.Error(err)
+//	}
+//	t.Log(transferNFTResponse)
 //}
